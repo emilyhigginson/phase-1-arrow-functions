ef04a0e7d2ab9e723bdfe85a01aaefc6e8b6bd81
Emily <emilyhigginson14@gmail.com>
2022-01-10 17:15:31 -0500

    commit tree c0a8d788a7f2007e8ce1720aa862453a7daefe4b

---
+++ index.js
@@ -0,0 +1,9 @@
+const divide = function() {
+    return 2000/100
+}
+const square = function (number) {
+    return (number*number)
+}
+const add = function (num1, num2) {
+    return num1 + num2
+}
